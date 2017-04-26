294e65304169e123179f378db188b6185636e9b1
hgh <453668907@qq.com>
2017-04-26 11:32:00 +0800

    数据源导入    --黄光辉
    Signed-off-by: hgh <453668907@qq.com>

--- check/src/main/java/com/zs/service/SourceImportSer.java
+++ check/src/main/java/com/zs/service/SourceImportSer.java
@@ -1,7 +1,10 @@
 package com.zs.service;
 
+import java.util.List;
+
 import com.zs.entity.SourceImport;
 
 public interface SourceImportSer extends BaseService<SourceImport>{
 
+	public String importData(List<String[]> list);
 }
